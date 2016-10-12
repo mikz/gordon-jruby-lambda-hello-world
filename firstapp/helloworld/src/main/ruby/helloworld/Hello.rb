de4c0598489781f0eef260282bb0f22b202e41dc
Michal Cichra <michal@cichra.cz>
2016-10-12 23:13:20 +0200

    expose through api gateway

--- firstapp/helloworld/src/main/ruby/helloworld/Hello.rb
+++ firstapp/helloworld/src/main/ruby/helloworld/Hello.rb
@@ -1,12 +1,14 @@
 java_package 'helloworld'
 java_import 'com.amazonaws.services.lambda.runtime.Context'
+java_import 'java.util.Map'
 
 class Hello
   become_java!
-  java_signature 'String handler(String, Context)'
+  java_signature 'String handler(Map<String,Object>, Context)'
 
-  def self.handler(event, context)
+  def self.handler(input, context)
     puts "hello"
+    puts input.inspect
     "hello world"
   end
 end
